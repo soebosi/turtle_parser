0b035667a63e2b5ec043c2bc17965b5778d28b8b
ebosi38051 <ebosi38051@gmail.com>
2017-06-19 23:57:56 +0900

    Implement statement parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use]
+extern crate nom;
+
+use nom::{IResult};
+
+named!(statement, take_until_either_and_consume!("."));
+
 fn main() {
-    println!("Hello, world!");
+    let input = &b"a b c. d e f."[..];
+    assert_eq!(statement(input), IResult::Done(&b" d e f."[..], &b"a b c"[..]));
 }
