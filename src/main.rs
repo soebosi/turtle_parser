267f71b051f0648b30e2f3cfbc2a2718dd6cf4c0
ebosi38051 <ebosi38051@gmail.com>
2017-06-21 00:17:12 +0900

    Implement pn_local_esc, percent

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 #[macro_use]
 extern crate nom;
 
-use nom::{IResult};
+use nom::{IResult, space};
 
 named!(statement, take_until_either_and_consume!("."));
 
+named!(percent, do_parse!(
+    char!('%')        >>
+    a: take!(2)       >>
+    (a)
+));
+named!(pn_local_esc, escaped!(
+    call!(space),
+    '\\',
+    one_of!("_~.-!$&\'()*+,;=/?#@%")
+));
+
 fn main() {
-    let input = &b"a b c. d e f."[..];
-    assert_eq!(statement(input), IResult::Done(&b" d e f."[..], &b"a b c"[..]));
+    assert_eq!(statement(&b"a b c. d e f."[..]), IResult::Done(&b" d e f."[..], &b"a b c"[..]));
+    assert_eq!(percent(&b"%2Abc"[..])          , IResult::Done(&b"bc"[..]     , &b"2A"[..])   );
+    assert_eq!(pn_local_esc(&b"\\.a b c"[..])  , IResult::Done(&b"a b c"[..]  , &b"\\."[..])  );
 }
