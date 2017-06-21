0d5ff76685dc5f23a11a8680bcbdbd7163a3c39e
ebosi38051 <ebosi38051@gmail.com>
2017-06-22 00:07:55 +0900

    Implement hex parser

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ extern crate nom;
 use nom::{IResult, space};
 
 named!(statement, take_until_either_and_consume!("."));
-
-named!(percent, do_parse!(
-    char!('%')        >>
-    a: take!(2)       >>
-    (a)
+named!(percent<(char, char)>, do_parse!(
+    tag!("%") >>
+    a: hex    >>
+    b: hex    >>
+    (a, b)
 ));
+named!(hex<char>, one_of!("0123456789ABCDEFabcdef"));
 named!(pn_local_esc, escaped!(
     call!(space),
     '\\',
@@ -18,6 +19,7 @@ named!(pn_local_esc, escaped!(
 
 fn main() {
     assert_eq!(statement(&b"a b c. d e f."[..]), IResult::Done(&b" d e f."[..], &b"a b c"[..]));
-    assert_eq!(percent(&b"%2Abc"[..])          , IResult::Done(&b"bc"[..]     , &b"2A"[..])   );
-    assert_eq!(pn_local_esc(&b"\\.a b c"[..])  , IResult::Done(&b"a b c"[..]  , &b"\\."[..])  );
+    assert_eq!(percent(&b"%2Abc"[..]),           IResult::Done(&b"bc"[..]     , ('2', 'A')   ));
+    assert_eq!(hex(&b"Abc"[..])                , IResult::Done(&b"bc"[..]     , 'A'          ));
+    assert_eq!(pn_local_esc(&b"\\.a b c"[..])  , IResult::Done(&b"a b c"[..]  , &b"\\."[..]  ));
 }
