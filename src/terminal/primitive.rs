36066cf668ff2889ce89ed62df01114a75b4a0ef
ebosi38051 <ebosi38051@gmail.com>
2017-07-23 22:18:00 +0900

    Fix terminal::primitive tests

--- src/terminal/primitive.rs
+++ src/terminal/primitive.rs
@@ -13,10 +13,6 @@ fn is_ws(c: char) -> bool {
     u == 0x20 || u == 0x9 || u == 0xD || u == 0xA
 }
 
-fn is_anon(c: char) -> bool {
-    c == '[' || c == ']' || is_ws(c)
-}
-
 #[derive(PartialEq, Debug)]
 pub struct Langtag<'a> {
     language: &'a str,
@@ -154,20 +150,10 @@ named!(pub exponent<&str, Exponent>, do_parse!(
 named!(pub ws<&str, &str>, take_while_s!(is_ws));
 
 /* [162s] ANON */
-named!(pub anon<&str, &str>, verify!(
-    take_while_s!(is_anon),
-    |val:&str| {
-        let len = val.char_indices().count();
-        val.char_indices().all(|(idx, c)| {
-            if idx == 0 {
-                c == '['
-            } else if idx == len - 1 {
-                c == ']'
-            } else {
-                is_ws(c)
-            }
-        })
-    }
+named!(pub anon<&str, &str>, delimited!(
+    tag!("["),
+    take_while_s!(is_ws),
+    tag!("]")
 ));
 
 /* [170s] PERCENT */
@@ -289,16 +275,22 @@ mod test {
 
     #[test]
     fn ws_test() {
-        assert_eq!(ws(" 	\r\nrest"), IResult::Done("rest", " 	\r\n"));
+        let input    = " 	\r\nrest";
+        let expected = IResult::Done("rest", " 	\r\n");
+        assert_eq!(ws(input), expected);
     }
 
     #[test]
     fn anon_test() {
-        assert_eq!(anon("[ ]a"), IResult::Done("a", "[ ]"));
+        let input    = "[ 	\r\n]rest";
+        let expected = IResult::Done("rest", " 	\r\n");
+        assert_eq!(anon(input), expected);
     }
 
     #[test]
     fn percent_test() {
-        assert_eq!(percent("%2Abc"), IResult::Done("bc", "%2A"));
+        let input    = "%2Arest";
+        let expected = IResult::Done("rest", "%2A");
+        assert_eq!(percent(input), expected);
     }
 }
