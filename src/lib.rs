c029f99a2ea766e71e4a01151dbffe3e89038004
ebosi38051 <ebosi38051@gmail.com>
2017-07-24 21:52:51 +0900

    Add Plx enum

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,16 @@ pub mod terminal {
 use terminal::pn::pn_local_esc;
 use terminal::primitive::percent;
 
+#[derive(PartialEq, Debug)]
+pub enum Plx<'a> {
+    Percent(&'a str),
+    PnLocalEsc(&'a str),
+}
 /* [169s] PLX */
-named!(pub plx<&str, &str>, alt!(percent | pn_local_esc));
+named!(pub plx<&str, Plx>, alt!(
+    map!(percent     , Plx::Percent   ) |
+    map!(pn_local_esc, Plx::PnLocalEsc)
+));
 
 #[cfg(test)]
 mod test {
@@ -21,7 +29,7 @@ mod test {
 
     #[test]
     fn plx_test() {
-        assert_eq!(plx("%2Arest"), IResult::Done("rest", "%2A"));
-        assert_eq!(plx("\\.rest"), IResult::Done("rest", "\\."));
+        assert_eq!(plx("%2Arest"), IResult::Done("rest", Plx::Percent("%2A")));
+        assert_eq!(plx("\\.rest"), IResult::Done("rest", Plx::PnLocalEsc("\\.")));
     }
 }
