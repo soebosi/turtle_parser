d29f6d2b4f60f42af0c444f9d402506dc9820f41
ebosi38051 <ebosi38051@gmail.com>
2017-10-14 18:51:29 +0900

    Implement reading ttl-file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use]
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+mod grammar;
+
+use grammar::{TurtleParser, Rule};
+use pest::Parser;
+use std::{fs, env};
+use std::io::Read;
+
 fn main() {
-    println!("Not implemented");
+    let args: Vec<String> = env::args().collect();
+    let mut file = fs::File::open(&args[1]).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents);
+
+    let pairs = TurtleParser::parse_str(Rule::turtle_doc, &contents).unwrap_or_else(|e| panic!("{}", e));
+    for pair in pairs.flatten() {
+        let rule = format!("{:?}", pair.as_rule());
+        let text = pair.as_str().replace("\n", "\\n");
+        println!("{:<40}: {}", rule, text);
+    }
 }
