c360d2904aa51e9ed61c2127572734668bf069c9
ebosi38051 <ebosi38051@gmail.com>
2018-01-17 00:47:22 +0900

    Create turtle struct

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,16 @@ extern crate pest_derive;
 mod grammar;
 
 use grammar::{TurtleParser, Rule};
-use pest::Parser;
+use pest::{Parser, iterators, inputs};
 use std::{fs, env};
 use std::io::Read;
+
+#[derive(Debug)]
+struct Result {
+    rule: String,
+    text: String,
+    children: Vec<Result>,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,10 +23,23 @@ fn main() {
     let mut contents = String::new();
     file.read_to_string(&mut contents);
 
-    let pairs = TurtleParser::parse_str(Rule::turtle_doc, &contents).unwrap_or_else(|e| panic!("{}", e));
-    for pair in pairs.flatten() {
+    let pairs = TurtleParser::parse_str(Rule::turtle_doc, &contents)
+        .unwrap_or_else(|e| panic!("{}", e));
+    let mut result = Vec::new();
+    print_deep(pairs, &mut result);
+    println!("{:?}", result);
+}
+
+fn print_deep(pairs: iterators::Pairs<grammar::Rule, inputs::StrInput>, result: &mut Vec<Result>) {
+    for pair in pairs {
         let rule = format!("{:?}", pair.as_rule());
-        let text = pair.as_str().replace("\n", "\\n");
-        println!("{:<40}: {}", rule, text);
+        let text = String::from(pair.as_str());
+        let mut children = Vec::new();
+        print_deep(pair.into_inner(), &mut children);
+        result.push(Result {
+            rule,
+            text,
+            children,
+        });
     }
 }
